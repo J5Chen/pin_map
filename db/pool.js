897d46775e8d825ab200f7efbe85a5e4b4096332
j5chen <jimmy@DESKTOP-T75591Q.>
2024-12-16 23:03:45 -0500

    .env and psql

--- db/pool.js
+++ db/pool.js
@@ -1,13 +1,13 @@
 const { Pool } = require("pg");
 require('dotenv').config();
-console.log(process.env.PSQL_ROLE_NAME)
+
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 
 module.exports = new Pool({
   host: "localhost", // or wherever the db is hosted
   user: process.env.PSQL_ROLE_NAME,
-  database: "top_users",
+  database: "pin_map",
   password: process.env.PSQL_PASSWORD,
   port: 5432 // The default port
 });
---
+++ db/populatedb.js
@@ -0,0 +1,37 @@
+const { Client } = require("pg");
+require("dotenv").config({ path: '../.env' });
+
+console.log(process.env);
+const SQL =  `
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR (200)
+);
+
+INSERT INTO users (name) 
+VALUES ('jimmy');
+
+CREATE TABLE IF NOT EXISTS pins (
+    pin_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    longitude DECIMAL(9, 6),
+    latitude DECIMAL(9, 6),
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+INSERT INTO pins (longitude, latitude, user_id)
+VALUES (45,45,1);
+`;
+
+async function main() {
+    console.log("seeding...");
+    const client = new Client({
+        connectionString: `postgresql://${process.env.PSQL_ROLE_NAME}:${process.env.PSQL_ROLE_PASSWORD}@localhost:5432/pin_maps`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("done");
+}
+
+main();
